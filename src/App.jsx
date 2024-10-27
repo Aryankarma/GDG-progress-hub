301d7645934382bc087331a1894cd8a3be47133e
Yahya Ansari <sayahya.ansari@gmail.com>
2024-10-27 16:33:42 +0530

    initial commit from yahya

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function App() {
   return (
     <div className="container">
-      <h1 className="text-3xl font-bold">Hello world!</h1>
+      <h1 className="text-3xl font-bold">Hello world! Hello GDG!</h1>
     </div>
   );
 }
