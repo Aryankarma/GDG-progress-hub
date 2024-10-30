e8d9ed272b351f79b9d50ba2337ccd7a003ad847
Utkarsh Srivastava <utkarshsrivastava1422@gmail.com>
2024-10-30 12:12:41 +0530

    firebase key remove

--- src/firebaseconfig.js
+++ src/firebaseconfig.js
@@ -3,12 +3,13 @@ import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBf4PQQbyvh-9IJ_8tTyjTTHsqPAjKkW1k",
-  authDomain: "gdg-admin-panel.firebaseapp.com",
-  projectId: "gdg-admin-panel",
-  storageBucket: "gdg-admin-panel.appspot.com",
-  messagingSenderId: "464259159067",
-  appId: "1:464259159067:web:3290861b124a6a980494be"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
