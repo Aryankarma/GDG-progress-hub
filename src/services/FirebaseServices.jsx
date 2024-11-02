2e4935f7da3bfea6ffde520ebfba4d70384ce31d
abhishekhojha <abhishekhojha45@gmail.com>
2024-11-03 02:53:08 +0530

    admin login and data with context

--- src/services/FirebaseServices.jsx
+++ src/services/FirebaseServices.jsx
@@ -37,26 +37,3 @@ export const fetchTeam = async () => {
     throw new Error("Failed to fetch team data");
   }
 };
-export const updateMembersScores = async (teamName, memberName, newScore) => {
-  try {
-    const teamDoc = doc(db, "testData", teamName);
-    const teamSnapshot = await getDoc(teamDoc);
-
-    if (teamSnapshot.exists()) {
-      const teamData = teamSnapshot.data();
-      const updatedMembers = teamData.members.map((member) => {
-        if (member.name === memberName) {
-          return { ...member, currentScore: newScore };
-        }
-        return member;
-      });
-
-      await updateDoc(teamDoc, { members: updatedMembers });
-    } else {
-      throw new Error("Team not found.");
-    }
-  } catch (error) {
-    console.error("Error updating score:", error);
-    throw new Error("Score update failed.");
-  }
-};
