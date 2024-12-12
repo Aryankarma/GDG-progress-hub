3d63fcaa2f6ab14551847e33db16e60ad5ddc502
Raushan Raj <164464654+raju-raushan@users.noreply.github.com>
2024-12-12 11:38:31 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,21 @@
-import "../styles/navbar.css"
+import { ModeToggle } from "../components/mode-toggle";
+import { ThemeProvider } from "../components/theme-provider";
+import "../styles/navbar.css";
 
 export default function Navbar() {
   return (
-    <div className="fixed bg-white w-full shadow-md">
-      <header className="myNavbar" >
-        <img src="./logo.png" alt="Google Developer Logo" />
-        <h3 className="text-slate-700">GDG Progress Hub</h3>
-      </header>
-    </div>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="fixed w-full shadow-md">
+        <header className="myNavbar">
+        <div className="flex-grow pr-4 text-left">
+            </div>
+            <img src="./logo.png" alt="Google Developer Logo" />
+            <h3 className="text-black dark:text-white">GDG Progress Hub</h3>
+            <div className="flex-grow pr-4 text-right">
+              <ModeToggle />
+            </div>
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
