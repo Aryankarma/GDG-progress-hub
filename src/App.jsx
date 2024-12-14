55228ea8803a2468719d85d5195a9b261b453c6e
Raushan Raj <164464654+raju-raushan@users.noreply.github.com>
2024-12-14 13:41:11 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import UserDashBoard from "./pages/userDashBoard";
-import AdmindashBoard from "./pages/adminDashboard";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Login from "./auth/login";
+import { ThemeProvider } from "./components/theme-provider";
 import { TeamProvider } from "./context/loginContext";
+import AdmindashBoard from "./pages/adminDashboard";
+import UserDashBoard from "./pages/userDashBoard";
+
 export default function App() {
   return (
-    <TeamProvider>
-      <Router>
-        <Routes>
-          <Route exact path="/" element={<UserDashBoard />} />
-          <Route exact path="/admin" element={<AdmindashBoard />} />
-          <Route exact path="/login" element={<Login />} />
-        </Routes>
-      </Router>
-    </TeamProvider>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <TeamProvider>
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<UserDashBoard />} />
+            <Route exact path="/admin" element={<AdmindashBoard />} />
+            <Route exact path="/login" element={<Login />} />
+          </Routes>
+        </Router>
+      </TeamProvider>
+    </ThemeProvider>
   );
 }
